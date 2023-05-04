3bd23d91ada2cd6e61249721bf255324a640c8ca
liangxianmai <liangxianmai@yahoo.com>
2023-05-04 15:08:03 -0400

    everything

---
+++ routes/gpt.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User')
+
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+const openai = new OpenAIApi(configuration);
+
+
+isLoggedIn = (req,res,next) => {
+  if (res.locals.loggedIn) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
+
+// generate page before any prompt has been typed
+router.get('/GPT',
+  isLoggedIn,
+  async (req, res, next) => {
+        const response = ""
+        res.render('GPT',{response});
+});
+
+
+/* generate the response and render page */
+router.post('/GPT',
+  isLoggedIn,
+  async (req, res, next) => {
+    const gptResponse = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Write a paraphrase using the following prompt: " + req.body.prompt,
+        max_tokens: 2048,
+    });
+    const response = gptResponse.data.choices[0].text
+    res.render('GPT',{response});
+});
+
+module.exports = router;
